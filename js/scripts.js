819be5679d2f5749d452c827d968bf5ebe4bb296
Jon Stump <jmstump@gmail.com>
2021-01-19 11:19:45 -0800

    add logic to displace the array after clicking submit
    Co-authored-by: Diego Salazar <salazardiego343@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,16 @@ let favoriteArray =[]
 
 $(document).ready(function() {
   $("#favorites").submit(function(event) {
+    $("#result").hide();
     event.preventDefault();
     const favorite1 = $("input#favorite1").val();
     const favorite2 = $("input#favorite2").val();
     const favorite3 = $("input#favorite3").val();
     favoriteArray.push(favorite1, favorite2, favorite3);
-
+      
+    $("#result").html(favoriteArray + " ");
     console.log(favoriteArray);
-  })
-})
+
+    $("#result").show();
+  });
+});
